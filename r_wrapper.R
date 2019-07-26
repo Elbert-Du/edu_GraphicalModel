5cd0e357086b21a5cdcd24822df15c40511cab3f
Elbert-Du <elbertdu100@gmail.com>
2019-07-26 16:44:18 -0400

    edited

--- r_wrapper.R
+++ r_wrapper.R
@@ -1,7 +1,8 @@
 library(reticulate)
 library(rjson)
 #library("PSIlence")
-setwd("GraphicalModel/edu_GraphicalModel")
+#setwd("GraphicalModel/edu_GraphicalModel")
+source("query_selection.R")
 make_gm <- import("match3")
 
 #When we actually use this, read these values from PSI interface
@@ -19,18 +20,15 @@ specs = fromJSON(path_to_specs)
 domain = fromJSON(path_to_domain)
 mapping = fromJSON(path_to_mapping)
 
-
-wrapper <- function(data, specs, domain, mapping, epsilon, delta) {
+num_iters <- 1000
+wrapper <- function(data, specs, domain, mapping, epsilon, delta, num_iters) {
   n = dim(data)[1]
-  first_measurements <- make_gm$shrink_domain(data, epsilon/3, delta)
-  measurements = first_measurements[1]
-  supports = first_measurements[2]
-  new_data = make_gm$transform_data(data, domain, supports)
-  compressed_data = new_data[1]
-  compressed_domain = new_data[2]
+  mech <- make_gm$Match3(data, specs, domain, mapping, iters = num_iters, warmup = TRUE)
+  new_data <- mech$shrink_domain(data, epsilon/3, delta)
+  compressed_data = first_measurements[1]
+  compressed_domain = first_measurements[2]
   queries = select_queries(compressed_data, n, sensitivity = 1, domain = compressed_domain, epsilon = epsilon/3, delta = delta)
-  mech <- make_gm$Match3(data, specs, domain, mapping, measurements, supports, queries)
-  mech$measure()
+  mech$measure(queries)
   mech$postprocess()
   mech$write_output()
 }
